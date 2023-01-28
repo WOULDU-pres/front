6be8583bc282ae6e520c05e155f32c149156d4a0
WOULDU-pres <jhw664089@gmail.com>
2023-01-28 22:27:06 +0900

    photo app not complete

--- src/App.js
+++ src/App.js
@@ -1,65 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import {
-  View,
-  Button,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-import Navigation from './navigations/Navigation';
+import Navigation from './navigations';
+import { LogBox } from 'react-native';
+import { UserProvier } from './contexts/UserContext';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 
 const App = () => {
-  // 이미지를 저장할 state 변수 선언
-  const [images, setImages] = useState([]);
-
-  // 이미지 선택 함수
-  const selectImages = async () => {
-    // 이미지 피커를 열고 선택된 이미지를 result 변수에 저장
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      // 다중 선택 허용
-      allowsMultipleSelection: true,
-    });
-    // 선택이 취소되지 않았다면
-    if (!result.canceled) {
-      // state 변수에 선택된 이미지 저장
-      setImages(result.assets);
-    }
-  };
+  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="dark" />
-      <Navigation />
-      <TouchableOpacity onPress={selectImages}>
-        <Image
-          source={require('../assets/gallery.jpeg')}
-          style={{ width: 200, height: 200 }}
-        />
-      </TouchableOpacity>
-      {images.map((image, index) => (
-        <Image key={index} source={{ uri: image.uri }} style={styles.image} />
-      ))}
-    </View>
+    <ActionSheetProvider>
+      <UserProvier>
+        <StatusBar style={'dark'} />
+        <Navigation />
+      </UserProvier>
+    </ActionSheetProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    marginTop: 20,
-  },
-  image: {
-    width: 200,
-    height: 200,
-    marginTop: 20,
-  },
-});
 
 export default App;
--- src/navigations/AuthStack.js
+++ src/navigations/AuthStack.js
@@ -1,7 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { WHITE } from '../colors';
 import SignInScreen from '../screens/SignInScreen';
-import SignUpScreen from '../screens/SIgnUpScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 import { AuthRoutes } from './routes';
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +9,10 @@ const Stack = createNativeStackNavigator();
 const AuthStack = () => {
   return (
     <Stack.Navigator
-      screenOptions={{ contentStyle: { backgrountColor: WHITE } }}
+      screenOptions={{
+        contentStyle: { backgroundColor: WHITE },
+        headerShown: false,
+      }}
     >
       <Stack.Screen name={AuthRoutes.SIGN_IN} component={SignInScreen} />
       <Stack.Screen name={AuthRoutes.SIGN_UP} component={SignUpScreen} />
