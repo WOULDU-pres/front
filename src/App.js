30300b07d4a5d79af779365a992a7f624b86df9a
WOULDU-pres <jhw664089@gmail.com>
2023-01-31 23:58:52 +0900

    restartUI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import Navigation from './navigations';
-import { LogBox } from 'react-native';
-import { UserProvier } from './contexts/UserContext';
-import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
-  LogBox.ignoreLogs(['AsyncStorage has been extracted from react-native core']);
-
+export default function App() {
   return (
-    <ActionSheetProvider>
-      <UserProvier>
-        <StatusBar style={'dark'} />
-        <Navigation />
-      </UserProvier>
-    </ActionSheetProvider>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- src/api/firebase.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from '../../env';
-
-export const initFirebase = () => {
-  return initializeApp(firebaseConfig);
-};
